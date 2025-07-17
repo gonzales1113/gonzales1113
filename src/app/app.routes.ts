a3dbfb8dc2410f8dabd3bdf6ba8c83ec74c15fb2
Mailee G. <114550065+gonzales1113@users.noreply.github.com>
2025-07-17 17:28:24 +0000

    adding homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  // your other routes here...
+
+  { path: '', component: Homepage },        // root path loads Homepage
+  { path: '**', component: Homepage }       // catch-all also loads Homepage
+];
